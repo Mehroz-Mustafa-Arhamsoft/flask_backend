965f7003d26172d85925b12f2db657950ffbd65c
Hassan Ahmad <hassansys@gmail.com>
2024-07-09 22:02:23 +0500

    Added Setup scripts

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# TODO 
+# Add Error Checking
+# Add return codes
+# Add code to check if python installed version is greater than 3.10
+
+echo "Creating Virtual requirement"
+python -m venv venv
+
+echo "Activating Virtual Enviroment ..."
+source venv/bin/activate
+
+echo "Installing required packages ..."
+pip install -r requirements.txt
+
+echo "Setup complete. Virtual environment is ready."
